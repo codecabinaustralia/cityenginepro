358731e85740123fd4ad55972c88c6c522909b8d
Josh Edgar <josh@codecabin.com.au>
2018-12-13 23:30:06 +1000

    update people relationship with student levels

--- app/models/ds_links_student_level.rb
+++ app/models/ds_links_student_level.rb
@@ -1,5 +1,5 @@
 class DsLinksStudentLevel < ApplicationRecord
-  belongs_to :ds_links_person, required: false
+  belongs_to :ds_links_people, required: false
   belongs_to :ds_links_level, required: false
   
 end
